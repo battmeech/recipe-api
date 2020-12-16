000fd18303ee9bcd8e7ecc4316433bdc1f70e3cc
Matt Beech <matthew_beech@live.co.uk>
2020-12-16 09:20:33 +0000

    Added a test for the validation of recipes

---
+++ lib/validation/__test__/recipeValidation.test.ts
@@ -0,0 +1,87 @@
+import { Recipe } from '../../models/recipe';
+import { newRecipeRules } from '../recipeValidation';
+
+describe('Tests of the recipe validation service', () => {
+    it('A valid recipe succeeds validation', () => {
+        // Setup
+        const recipeToTest: Recipe = {
+            cookingTime: '20 mins',
+            description: 'This is a test recipe',
+            difficulty: 'HARD',
+            ingredients: [],
+            method: [],
+            name: 'Test recipe',
+            prepTime: '5 mins',
+            serves: 1,
+        };
+
+        // Run test
+        const { error } = newRecipeRules.validate(recipeToTest);
+
+        // Assert
+        expect(error).toBeFalsy();
+    });
+
+    it('A recipe with a missing field gives a validation error', () => {
+        // Setup
+        const recipeToTest = {
+            cookingTime: '20 mins',
+            description: 'This is a test recipe',
+            difficulty: 'HARD',
+            ingredients: [],
+            method: [],
+            prepTime: '5 mins',
+            serves: 1,
+        };
+
+        // Run test
+        const { error } = newRecipeRules.validate(recipeToTest);
+
+        // Assert
+        expect(error?.message).toStrictEqual('"name" is required');
+    });
+
+    it('A recipe with an invalid field gives a validation error', () => {
+        // Setup
+        const recipeToTest: Recipe = {
+            cookingTime: '20 mins',
+            description: 'This is a test recipe',
+            difficulty: 'HARD',
+            ingredients: [],
+            method: [],
+            name: 'a',
+            prepTime: '5 mins',
+            serves: 1,
+        };
+
+        // Run test
+        const { error } = newRecipeRules.validate(recipeToTest);
+
+        // Assert
+        expect(error?.message).toStrictEqual(
+            '"name" length must be at least 2 characters long'
+        );
+    });
+
+    it('A recipe with an invalid nested object gives a validation error', () => {
+        // Setup
+        const recipeToTest = {
+            cookingTime: '20 mins',
+            description: 'This is a test recipe',
+            difficulty: 'HARD',
+            ingredients: [{ quantityType: 'g', name: 'Cheese' }],
+            method: [],
+            name: 'ab',
+            prepTime: '5 mins',
+            serves: 1,
+        };
+
+        // Run test
+        const { error } = newRecipeRules.validate(recipeToTest);
+
+        // Assert
+        expect(error?.message).toStrictEqual(
+            '"ingredients[0].quantity" is required'
+        );
+    });
+});
