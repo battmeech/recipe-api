8e1a4b2b34845383ffe7787871cfdaa041019c50
Matt Beech <matthew_beech@live.co.uk>
2021-01-31 19:52:55 +0000

    Updated to use the correct paths

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
+import { Config } from './config';
 import { router } from './routes/router';
 
 const app = express();
@@ -7,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(express.static('client/build'));
+
+if (Config.env === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 export { app };
